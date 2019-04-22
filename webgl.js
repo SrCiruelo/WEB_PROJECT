97a9f3cccea8c10e19fb6af6a2193ece11dfb30e
SrCiruelo <43975321+SrCiruelo@users.noreply.github.com>
2019-04-22 21:28:29 +0200

    This may won't work (Changes Needed)

--- webgl.js
+++ webgl.js
@@ -1,78 +1,175 @@
 //webgl.js
-var positions0 = [
-    30,30,
-    100,30,
-    30,100,
+var vertex_array ={
+    0,0,0, //0
+    0,100,0, //1
+    100,100,0, //2
+    100,0,0, //3
+    100,0,100, //4
+    100,100,100, //5
+    0,100,100, //6
+    0,0,100 //7
+}
+var triangles_array = {
+    0,1,6,
+    0,7,6,
+    0,1,2,
+    0,2,3,
+    3,5,4,
+    3,2,5,
+    7,6,5,
+    7,5,4,
+    1,6,2,
+    6,2,5,
+    0,3,7,
+    3,7,4
+}
 
-];
-var positions1 = [
-    100,30,
-    30,100,
-    100,100
-    
-];
-var m3 = {
-      translation: function(tx, ty) {
-        return [
-          1, 0, 0,
-          0, 1, 0,
-          tx, ty, 1,
-        ];
-      },
-     
-      rotation: function(angleInRadians) {
-        var c = Math.cos(angleInRadians);
-        var s = Math.sin(angleInRadians);
-        return [
-          c,-s, 0,
-          s, c, 0,
-          0, 0, 1,
-        ];
-      },
-     
-      scaling: function(sx, sy) {
-        return [
-          sx, 0, 0,
-          0, sy, 0,
-          0, 0, 1,
-        ];
-      },
-    multiply: function(a, b) {
-    var a00 = a[0 * 3 + 0];
-    var a01 = a[0 * 3 + 1];
-    var a02 = a[0 * 3 + 2];
-    var a10 = a[1 * 3 + 0];
-    var a11 = a[1 * 3 + 1];
-    var a12 = a[1 * 3 + 2];
-    var a20 = a[2 * 3 + 0];
-    var a21 = a[2 * 3 + 1];
-    var a22 = a[2 * 3 + 2];
-    var b00 = b[0 * 3 + 0];
-    var b01 = b[0 * 3 + 1];
-    var b02 = b[0 * 3 + 2];
-    var b10 = b[1 * 3 + 0];
-    var b11 = b[1 * 3 + 1];
-    var b12 = b[1 * 3 + 2];
-    var b20 = b[2 * 3 + 0];
-    var b21 = b[2 * 3 + 1];
-    var b22 = b[2 * 3 + 2];
+var m4 = {
+
+  projection: function(width, height, depth) {
+    // Note: This matrix flips the Y axis so 0 is at the top.
     return [
-      b00 * a00 + b01 * a10 + b02 * a20,
-      b00 * a01 + b01 * a11 + b02 * a21,
-      b00 * a02 + b01 * a12 + b02 * a22,
-      b10 * a00 + b11 * a10 + b12 * a20,
-      b10 * a01 + b11 * a11 + b12 * a21,
-      b10 * a02 + b11 * a12 + b12 * a22,
-      b20 * a00 + b21 * a10 + b22 * a20,
-      b20 * a01 + b21 * a11 + b22 * a21,
-      b20 * a02 + b21 * a12 + b22 * a22,
+       2 / width, 0, 0, 0,
+       0, -2 / height, 0, 0,
+       0, 0, 2 / depth, 0,
+      -1, 1, 0, 1,
     ];
   },
+
+  multiply: function(a, b) {
+    var a00 = a[0 * 4 + 0];
+    var a01 = a[0 * 4 + 1];
+    var a02 = a[0 * 4 + 2];
+    var a03 = a[0 * 4 + 3];
+    var a10 = a[1 * 4 + 0];
+    var a11 = a[1 * 4 + 1];
+    var a12 = a[1 * 4 + 2];
+    var a13 = a[1 * 4 + 3];
+    var a20 = a[2 * 4 + 0];
+    var a21 = a[2 * 4 + 1];
+    var a22 = a[2 * 4 + 2];
+    var a23 = a[2 * 4 + 3];
+    var a30 = a[3 * 4 + 0];
+    var a31 = a[3 * 4 + 1];
+    var a32 = a[3 * 4 + 2];
+    var a33 = a[3 * 4 + 3];
+    var b00 = b[0 * 4 + 0];
+    var b01 = b[0 * 4 + 1];
+    var b02 = b[0 * 4 + 2];
+    var b03 = b[0 * 4 + 3];
+    var b10 = b[1 * 4 + 0];
+    var b11 = b[1 * 4 + 1];
+    var b12 = b[1 * 4 + 2];
+    var b13 = b[1 * 4 + 3];
+    var b20 = b[2 * 4 + 0];
+    var b21 = b[2 * 4 + 1];
+    var b22 = b[2 * 4 + 2];
+    var b23 = b[2 * 4 + 3];
+    var b30 = b[3 * 4 + 0];
+    var b31 = b[3 * 4 + 1];
+    var b32 = b[3 * 4 + 2];
+    var b33 = b[3 * 4 + 3];
+    return [
+      b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+      b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+      b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+      b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+      b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+      b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+      b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+      b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+      b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+      b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+      b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+      b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+      b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+      b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+      b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+      b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+    ];
+  },
+
+  translation: function(tx, ty, tz) {
+    return [
+       1,  0,  0,  0,
+       0,  1,  0,  0,
+       0,  0,  1,  0,
+       tx, ty, tz, 1,
+    ];
+  },
+
+  xRotation: function(angleInRadians) {
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    return [
+      1, 0, 0, 0,
+      0, c, s, 0,
+      0, -s, c, 0,
+      0, 0, 0, 1,
+    ];
+  },
+
+  yRotation: function(angleInRadians) {
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    return [
+      c, 0, -s, 0,
+      0, 1, 0, 0,
+      s, 0, c, 0,
+      0, 0, 0, 1,
+    ];
+  },
+
+  zRotation: function(angleInRadians) {
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    return [
+       c, s, 0, 0,
+      -s, c, 0, 0,
+       0, 0, 1, 0,
+       0, 0, 0, 1,
+    ];
+  },
+
+  scaling: function(sx, sy, sz) {
+    return [
+      sx, 0,  0,  0,
+      0, sy,  0,  0,
+      0,  0, sz,  0,
+      0,  0,  0,  1,
+    ];
+  },
+
+  translate: function(m, tx, ty, tz) {
+    return m4.multiply(m, m4.translation(tx, ty, tz));
+  },
+
+  xRotate: function(m, angleInRadians) {
+    return m4.multiply(m, m4.xRotation(angleInRadians));
+  },
+
+  yRotate: function(m, angleInRadians) {
+    return m4.multiply(m, m4.yRotation(angleInRadians));
+  },
+
+  zRotate: function(m, angleInRadians) {
+    return m4.multiply(m, m4.zRotation(angleInRadians));
+  },
+
+  scale: function(m, sx, sy, sz) {
+    return m4.multiply(m, m4.scaling(sx, sy, sz));
+  },
+
 };
 
-var angle = 0;
-var trans = [0,0];
-var scale = [1,1];
+var angle_x = 0;
+var angle_y = 0;
+var angle_z = 0;
+var trans = [0,0,0];
+var scale = [1,1,1];
 
 var change_rotation = function(x){
     angle = x/50 * Math.PI;
@@ -96,7 +193,7 @@ var y_change_scale = function(y){
 }
 
 var draw_scene;
-var canvas0
+var canvas0;
 var main = function(){
 
     //Get Canvas
@@ -128,7 +225,7 @@ var main = function(){
     //Now we can enter data refering to the bind point
     //we refer to our bind point, create a compatible binary array by copy that webgl can work with
     //gl.STATIC_DRAW says we are not going to change the data much("STATIC")
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions0), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(), gl.STATIC_DRAW);
     //We need set to the Display size our size in the html
     //webgl-utils.resizeCanvasToDisplaySize(gl.canvas);
     //This tells WebGl the -1 to +1 clip space maps to 0 gl.canvas.width and height
@@ -150,28 +247,33 @@ var main = function(){
         var normalize = false; // don't normalize the data
         var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
         var offset = 0;        // start at the beginning of the buffer
-        //this tells atributtes of the variables 
-        //It Binds positionAttributeLocation to ARRAY_BUFFER 
-        gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
-        gl.uniform2f(resolutionUniformLocation,gl.canvas.width,gl.canvas.height);
-        gl.uniform4f(colorUniformLocation,0.1,0.9,0.2,1);
-        var translationMatrix = m3.translation(trans[0],trans[1]);
-        var rotationMatrix = m3.rotation(angle);
-        var scaleMatrix = m3.scaling(scale[0],scale[1]);
         
-         var matrix = m3.multiply(translationMatrix, rotationMatrix);
-        matrix = m3.multiply(matrix, scaleMatrix);
-        
-        gl.uniformMatrix3fv(matrixUniformLocation, false, matrix);
-        // draw
         var primitiveType = gl.TRIANGLES;
         var offset = 0;
         var count = 3;
-        gl.drawArrays(primitiveType, offset, count);
-        gl.uniform4f(colorUniformLocation,0.4,0.7,0.5,1);
-
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions1), gl.STATIC_DRAW);
-         gl.drawArrays(primitiveType, offset, count);
+        
+        var matrix = m4.projection(gl.canvas.clientWidth, gl.canvas.clientHeight, 400);
+        matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
+        matrix = m4.xRotate(matrix, rotation[0]);
+        matrix = m4.yRotate(matrix, rotation[1]);
+        matrix = m4.zRotate(matrix, rotation[2]);
+        matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
+        
+        for(var i=0;i<triangles_array.length;i+3)
+        {
+            
+            var positions = {vertex_array[triangles_array[i]],vertex_array[triangles_array[i+1]],vertex_array[triangles_array[i+2]]};
+            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+            gl.vertexAttribPointer(positionLocation, size, type, normalize, stride, offset);
+            
+            gl.uniform2f(resolutionUniformLocation,gl.canvas.width,gl.canvas.height);
+            gl.uniform4f(colorUniformLocation,0.1,0.9,0.2,1);
+            gl.uniformMatrix4fv(matrixUniformLocation, false, matrix);
+            
+            gl.drawArrays(primitiveType, offset, count);
+        }
+        
     }
         draw_scene();
     //draw_scene();
